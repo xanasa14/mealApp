824bf9d74ce883c974841250893dd703451eb343
xanasa14 <xanasa14@hotmail.com>
2022-09-09 23:29:09 -0500

    starting to navigation component

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import CategoriesScreen from './screens/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <StatusBar style="dark" />    
+      <NavigationContainer >
+        <Stack.Navigator>
+          <Stack.Screen 
+          name="MealsCategories" 
+          component={CategoriesScreen}
+          />
+        </Stack.Navigator>
+        <CategoriesScreen />
+      </NavigationContainer>
+    </>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container:{}
 });
