cee95eaf87c36f6044f791a6fc1ccb76e4d62eed
xanasa14 <xanasa14@hotmail.com>
2022-09-11 20:53:46 -0500

    Passing data to different Screens part 1

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -6,7 +6,7 @@ import { CATEGORIES } from '../data/dummy-data';
 function CategoriesScreen({ navigation }) {
   function renderCategoryItem(itemData) {
     function pressHandler() {
-      navigation.navigate('MealsOverview');
+      navigation.navigate('MealsOverview', {categoryId: itemData.item.id,});
     }
 
     return (
